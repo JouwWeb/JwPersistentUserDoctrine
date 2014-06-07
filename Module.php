0ed2c369e5867243b3dc083b106434d03349a41d
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2014-06-07 16:35:17 +0200

    Add Module skeleton.

---
+++ Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JwPersistentUserDoctrine;
+
+use Zend\ModuleManager\Feature;
+
+class Module implements
+    Feature\ConfigProviderInterface,
+    Feature\AutoloaderProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+}
